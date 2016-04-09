219f0a8ac4d8d26115246fd0eb31a8763bbc0a8c
Taro Aoki <aizu.s1230022@gmail.com>
2016-04-09 23:43:55 +0900

    [Fix] Recieved message process

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 "use strict";
 
-var request = require('request');
-var express = require('express');
-var app     = express();
-var port    = process.env.PORT || 5000;
+var request    = require('request');
+var bodyParser = require('body-parser');
+var express    = require('express');
+var app        = express();
+var port       = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
 app.post('/callback', function(req, res) {
-  let params = (JSON.parse(req.body)).result.content;
+  let result = req.body;
 
   let options = {
     url     : 'https://trialbot-api.line.me/v1/events',
+    method  : 'POST',
     headers : {
-      'Content-Type'                 : 'application/json; charser=UTF-8',
-      'X-Line-ChannelID'             : process.env.CHANNEL_ID,
-      'X-Line-ChannelSecret'         : process.env.CHANNEL_SECRET,
-      'X-Line-Trusted-User-With-ACL' : process.env.MID
+      Content-Type                 : 'application/json; charser=UTF-8',
+      X-Line-ChannelID             : process.env.CHANNEL_ID,
+      X-Line-ChannelSecret         : process.env.CHANNEL_SECRET,
+      X-Line-Trusted-User-With-ACL : process.env.MID
     }
   };
 
   let messages = {
-    'to'        : params.to[0],
-    'toChannel' : 1383378250,
-    'eventType' : '138311608800106203',
-    'content'   : {
-      'contentType' : 1,
-      'toType'      : 1,
-      'text'        : params.text
+    to        : [result.result[0].content.from],
+    toChannel : 1383378250,
+    eventType : '138311608800106203',
+    content   : {
+      contentType : 1,
+      toType      : 1,
+      text        : result.result[0].content.text
     },
-    'proxy'     : process.env.FIXIE_URL
+    proxy     : process.env.FIXIE_URL
   };
 
   request(options, function(error, res, body) {
