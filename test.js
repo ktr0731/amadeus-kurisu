c131790ecc7849cc72a1699ec1911fab89a53ea1
Taro Aoki <aizu.s1230022@gmail.com>
2016-04-22 18:24:52 +0900

    [Add] npm install geolib

--- test.js
+++
@@ -1,6 +0,0 @@
-var geolib = require('geolib');
-
-var p1 = { latitude : 137.01739992, longitude : 36.72362158 };
-var p2 = { latitude : 139.929810, longitude : 37.494761 };
-
-console.log(geolib.getDistance(p1, p2) / 1000);
